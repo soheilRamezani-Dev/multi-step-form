9c76b5561ba7de1df318135ceed75450efc8993c
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-12-24 14:24:44 +0330

    mobile navigation structur complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.scss";
+import Navigation from "./componentts/navigation";
 
 function App() {
 
 
   
-  return (<>
-      <div>navigation</div>
+  return (<div>
+    {/* Nsvigation */}
+      <Navigation/>
+
+
+      {/* body */}
       <div>body</div>
-      </>
+
+      
+      </div>
     );
 }
 
---
+++ src/componentts/navigation.tsx
@@ -0,0 +1,13 @@
+
+const Navigation = () => {
+    return <div className="navigation">
+        <ul className="navigation-list">
+            <li className="navigation-item active">1</li>
+            <li className="navigation-item">2</li>
+            <li className="navigation-item">3</li>
+            <li className="navigation-item">4</li>
+        </ul>
+    </div>
+}
+ 
+export default Navigation;
