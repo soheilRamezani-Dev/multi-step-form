ac208896ac28fafda76601b276446933b1956ae2
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-12-28 14:40:33 +0330

    add active step to navigation

--- src/componentts/navigation/navigation.tsx
+++ src/componentts/navigation/navigation.tsx
@@ -1,12 +1,22 @@
 import NavigationItem from "./NavigationItem";
 import steps from "../../api/steps";
+import { useLocation } from "react-router-dom";
 
 const Navigation = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
     <div className="navigation">
       <ul className="navigation-list">
-        {steps.map((step,index)=> <NavigationItem id={index+1} title={step.title} url={step.url} active />)}
-        
+        {steps.map((step, index) => (
+          <NavigationItem
+            id={index + 1}
+            title={step.title}
+            url={step.url}
+            active={pathname === step.url ? true : false}
+          />
+        ))}
+
         {/* <NavigationItem id={2} title={'SELECT PLAN'}/>
         <NavigationItem id={3} title={'ADD-ONS'}/>
         <NavigationItem id={4} title={'SUMMARY'}/> */}
